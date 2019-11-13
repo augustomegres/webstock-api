f1fd8e2c61525596717393fded4e7cfb38932fec
Jose Augusto Megres de Oliveira <augustomegres@gmail.com>
2019-11-13 02:46:02 -0300

    Ajustando variaveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: "postgres",
-  host: process.env.DATABASE_URL,
+  host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
