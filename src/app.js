63940605919b45c244e4b4f7f33a027e3fb9d3c4
Jose Augusto Megres de Oliveira <augustomegres@gmail.com>
2019-12-21 17:51:28 -0300

    Ajustando ordem

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
-require("dotenv").config();
-
-require("./database/index");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+require("./database/index");
 
 app.listen(process.env.PORT || 3333, () => {
   console.log("Servidor online!");
