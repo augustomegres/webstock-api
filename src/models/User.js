e368bbec6f2097e0ec076efad9d09af46416e154
Jose Augusto Megres de Oliveira <augustomegres@gmail.com>
2019-12-28 00:50:27 -0300

    Adicionando coluna para expiracao de token

--- src/models/User.js
+++ src/models/User.js
@@ -9,6 +9,7 @@ class User extends Model {
         cpf: DataTypes.STRING,
         date_of_birth: DataTypes.DATE,
         passwordHash: DataTypes.STRING,
+        recoverPasswordToken: DataTypes.STRING,
         isAdmin: DataTypes.BOOLEAN
       },
       {
