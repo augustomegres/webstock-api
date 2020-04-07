b1cfac6b74221c643145e4f3162e0ebdc69ef53a
Jose Augusto Megres de Oliveira <augustomegres@gmail.com>
2020-04-07 02:42:36 -0300

    [Test]Testando na umbler

--- src/app.js
+++ src/app.js
@@ -3,18 +3,7 @@ const routes = require("./routes");
 const cors = require("cors");
 const app = express();
 
-var whitelist = ['https://app.webstock.com.br']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
