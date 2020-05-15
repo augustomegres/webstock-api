f982e6911226bd2c19191db7a6537c26f3e83bc7
Jose Augusto Megres de Oliveira <augustomegres@gmail.com>
2020-05-14 23:37:57 -0300

    refactor(authcontroller): alteração da expiração do token para 1 semana

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 function generateToken(params = {}) {
   return jwt.sign(params, process.env.SECRET, {
-    expiresIn: 86400,
+    expiresIn: 604800,
   });
 }
 
