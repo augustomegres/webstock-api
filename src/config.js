e1df49bcf1d5b44c48ca3bdc6b0c6c18e3d90e0d
José Augusto Megres de Oliveira <augustomegres@gmail.com>
2020-08-10 16:14:57 -0300

    test: variavel no cloudbuild

--- src/config.js
+++ src/config.js
@@ -1,4 +1,12 @@
 // Import all env vars from .env file
 require("dotenv").config();
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PASS = process.env.DB_PASS;
+export const DB_PATH = process.env.DB_PATH;
 export const DB_URL = process.env.DB_URL;
-console.log(DB_URL); // => Hello
+export const DB_USER = process.env.DB_USER;
+export const PAGARME_KEY = process.env.PAGARME_KEY;
+export const SECRET = process.env.SECRET;
+export const SENDGRID = process.env.SENDGRID;
+console.log("VARIAVEL =>" + DB_URL);
