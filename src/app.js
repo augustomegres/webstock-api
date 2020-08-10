63b082fbf9b9c1c08ed7f7ba3337acd70c172d3e
José Augusto Megres de Oliveira <augustomegres@gmail.com>
2020-08-10 17:53:24 -0300

    test: config cloudbuild

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv")
+dotenv.config()
 
 app.use(cors());
 app.use(express.json());
